9ae59adaf5aab777446286e7f3f5ca8cff240030
ShaikhZai <shaikhmoh1173@gmail.com>
2023-06-18 21:41:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+//import React from 'react'; newer version o react you need not need to import react 
+import Hero from './components/Hero';
+import Demo from './components/Demo';
+import './App.css';
+
+const App = () => {
+  return (
+    <main>
+      <div className="main">
+        <div className="gradient"/>
+      </div>
+
+      <div className="app">
+          <Hero/>
+          <Demo/>
+      </div>
+    </main>
+  )
+}
+
+export default App
